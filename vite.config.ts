8732fc44c69eb159e38727f1af01619bebacbf79
chunglnt <chungle.drive.tomari@gmail.com>
2024-07-25 23:23:36 +0700

    update index

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/primevvue/',
+  base: '/primevvue/',
   plugins: [vue()],
   resolve: {
     alias: {
