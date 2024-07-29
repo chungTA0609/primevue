de2280a7daf986a5e69b53f48b252020ed04b678
chungTA0609 <50292147+chungTA0609@users.noreply.github.com>
2024-07-29 01:13:48 +0000

    update viteSingleFile

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/primevvue/',
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
